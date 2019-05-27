8690948ac4fade535f294a0ae9760847e321cd7b
Leonardo Quevedo <pac.leo@hotmail.com>
2019-05-27 15:34:55 -0300

    Updates run script.

--- src/lib/sync/capacitor/index.js
+++ src/lib/sync/capacitor/index.js
@@ -8,27 +8,26 @@ const shell = require('shelljs')
 const { exec } = shell
 const { log } = console
 
-module.exports = () =>
-  new Promise(async (resolve, reject) => {
-    try {
-      const rootPath = path.resolve(__dirname, '../../../')
-      const capacitorConfigFile = path.join(rootPath, 'capacitor.config.json')
-      /* eslint-disable-next-line */
-      const capacitorConfigContent = require(capacitorConfigFile)
-      const isLivereload = process.env.CAPBOX_LIVERELOAD
-      log(`Generating capacitor config....`.yellow)
-      if (isLivereload) {
-        const LIVERELOAD_IP = ip.address()
-        const LIVERELOAD_PORT = process.env.PORT
-        const LIVERELOAD_SERVER = `http://${LIVERELOAD_IP}:${LIVERELOAD_PORT}`
-        capacitorConfigContent.server = { url: LIVERELOAD_SERVER }
-      }
-      fs.writeFileSync(capacitorConfigFile, JSON.stringify(capacitorConfigContent, null, 2))
-      log(`Generating capacitor config....`.yellow)
-      await exec('npx cap sync')
-      log('Capacitor build updated successfully!'.green)
-      resolve()
-    } catch (e) {
-      reject(e)
+module.exports = new Promise(async (resolve, reject) => {
+  try {
+    const rootPath = path.resolve(__dirname, '../../../')
+    const capacitorConfigFile = path.join(rootPath, 'capacitor.config.json')
+    /* eslint-disable-next-line */
+    const capacitorConfigContent = require(capacitorConfigFile)
+    const isLivereload = process.env.CAPBOX_LIVERELOAD
+    log(`Generating capacitor config....`.yellow)
+    if (isLivereload) {
+      const LIVERELOAD_IP = ip.address()
+      const LIVERELOAD_PORT = process.env.PORT
+      const LIVERELOAD_SERVER = `http://${LIVERELOAD_IP}:${LIVERELOAD_PORT}`
+      capacitorConfigContent.server = { url: LIVERELOAD_SERVER }
     }
-  })
+    fs.writeFileSync(capacitorConfigFile, JSON.stringify(capacitorConfigContent, null, 2))
+    log(`Generating capacitor config....`.yellow)
+    await exec('npx cap sync')
+    log('Capacitor build updated successfully!'.green)
+    resolve()
+  } catch (e) {
+    reject(e)
+  }
+})
