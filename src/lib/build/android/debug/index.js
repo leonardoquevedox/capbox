f1ce72d6231ad87f56d7914a43746a18bfbc4b29
Leonardo Quevedo <pac.leo@hotmail.com>
2019-09-12 12:34:57 -0300

    Updates lib to 1.4.1.

--- src/lib/build/android/debug/index.js
+++ src/lib/build/android/debug/index.js
@@ -10,8 +10,12 @@ module.exports = new Promise(async (resolve, reject)=>{
   try{
     const rootPath = process.env.CAPACITOR_PROJECT_ROOT
     const androidPath = path.join(rootPath, 'android')
+    const isWin = process.platform === "win32"
     log.header(`Generating android build...`.yellow)
-    await exec(`./gradlew assembleDebug`, { cwd: androidPath })
+
+    if(isWin) await exec('gradlew assembleDebug', { cwd: androidPath })
+    else await exec('./gradlew assembleDebug', { cwd: androidPath })
+
     log.success(`Android built successfully!`.green.bold)
     resolve()
   } catch(e){
--- src/lib/build/android/release/index.js
+++ src/lib/build/android/release/index.js
@@ -10,9 +10,13 @@ module.exports = new Promise(async (resolve, reject)=>{
   try{
     const rootPath = process.env.CAPACITOR_PROJECT_ROOT
     const androidPath = path.join(rootPath, 'android')
-    log.header('Generating android build...'.yellow)
-    await exec('./gradlew assembleRelease', { cwd: androidPath })
-    log.success('Android built successfully!'.green.bold)
+    const isWin = process.platform === "win32"
+    log.header(`Generating android build...`.yellow)
+
+    if(isWin) await exec('gradlew assembleRelease', { cwd: androidPath })
+    else await exec('./gradlew assembleRelease', { cwd: androidPath })
+
+    log.success(`Android built successfully!`.green.bold)
     resolve()
   } catch(e){
     reject(e)
