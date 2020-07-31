8d20ce97f1554b2428e0a909b7379028bbff9516
Leonardo Quevedo <pac.leo@hotmail.com>
2020-07-31 11:24:50 -0300

    Adds fixes to sync script.

--- src/lib/sync/index.js
+++ src/lib/sync/index.js
@@ -1,19 +1,19 @@
 require('colors')
 
-import path from 'path';
-import paths from '../../utils/paths';
-import log from '../../utils/log';
+import path from 'path'
+import paths from '../../utils/paths'
+import log from '../../utils/log'
 
-export default () => new Promise(async (resolve, reject) => {
-  try {
-    const script = path.join(__dirname, 'capacitor')
-    const rootPath = paths.getRootPath()
-    log.header(`Preparing ${platform} build...`.yellow)
-    log.header(`Got build folder: ${rootPath}`)
-    log.header(`Running script ${script}`)
-    await require(script)({ rootPath })
-    resolve()
-  } catch (e) {
-    reject(e)
-  }
-});
+export default () =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const script = path.join(__dirname, 'capacitor')
+      const rootPath = paths.getRootPath()
+      log.header(`Got build folder: ${rootPath}`)
+      log.header(`Running script ${script}`)
+      await require(script)({ rootPath })
+      resolve()
+    } catch (e) {
+      reject(e)
+    }
+  })
